6bcb1e487c3782b0500c719154ec7cb9ca686673
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-11-17 11:33:42 +0700

    up

--- lib/src/screens/oder_screen.dart
+++ lib/src/screens/oder_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:example/flow/blocs/order_bloc.dart';
 import 'package:example/flow/state/order_state.dart';
+import 'package:example/src/models/food.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,15 +13,44 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<OrderScreen> {
+
+
+  late List<Food> foods;
+  @override
+  void initState() {
+    super.initState();
+  
+    
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<OrderBloc, OrderState>(
       listener: (context, state) {
+        
+
         if(state is OrderStateSuccess) {
-          print('data: ${state.foods}');
+          setState(() {
+            foods = state.foods;
+          });
+
         }
+
+        print('data======sdsdsdsdsdsdsd=====: ${foods}');
       },
-      child: Text('hung'),
+      child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Order'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(45, 182, 163, 100),
+        //iconTheme: const IconThemeData(color: Colors.black),
+        //actions: [],
+        automaticallyImplyLeading: false,
+        elevation: 0.0,
+      ),
+      body: Text('hung dep tai')
+    ),
     );
   }
 }
