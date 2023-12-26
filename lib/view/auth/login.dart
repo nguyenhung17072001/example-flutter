eb887d00391da0e7335ad624a29df2270bee54f9
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-26 17:34:39 +0700

    update

--- lib/view/auth/login.dart
+++ lib/view/auth/login.dart
@@ -17,61 +17,66 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        //color: Colors.black,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-              alignment: Alignment.center,
-              height: MediaQuery.of(context).size.width *0.5,
-              width: MediaQuery.of(context).size.width *0.5,
-              child: Image.asset(
-                'assets/logo.png',
-                fit: BoxFit.contain,
-              )
-            ),
-            Column(
-              children: [
-                TextInput(
-                  controller: _constructionUsernameValue,
-                  labelText: 'Tên đăng nhập',
-                  icon: Icons.account_circle,
-                ),
-                TextInput(
-                  controller: _constructionUPasswordValue,
-                  labelText: 'Mật khẩu',
-                  icon: Icons.lock,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                ),
-              ],
-            ),
-            Flexible(
-              child: Container(
-                margin: const EdgeInsets.all(30.0),
-                //height: 60,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor:  AppColors.primaryColor,
-                    foregroundColor: Colors.white,
-                    padding: const EdgeInsets.fromLTRB(60, 15, 60, 15)
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          //color: Colors.black,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.width *0.5,
+                width: MediaQuery.of(context).size.width *0.5,
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.contain,
+                )
+              ),
+              Column(
+                children: [
+                  TextInput(
+                    controller: _constructionUsernameValue,
+                    labelText: 'Tên đăng nhập',
+                    icon: Icons.account_circle,
                   ),
-                  onPressed: () {
-                    
-                  }, 
-                  child: const Text('Đăng nhập', style: TextStyle(color: Colors.white, fontSize: 14),)
-                ),
+                  TextInput(
+                    controller: _constructionUPasswordValue,
+                    labelText: 'Mật khẩu',
+                    icon: Icons.lock,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                ],
+              ),
+              Flexible(
+                child: Container(
+                  margin: const EdgeInsets.all(30.0),
+                  //height: 60,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor:  AppColors.primaryColor,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.fromLTRB(60, 15, 60, 15)
+                    ),
+                    onPressed: () {
+                      
+                    }, 
+                    child: const Text('Đăng nhập', style: TextStyle(color: Colors.white, fontSize: 14),)
+                  ),
+                )
               )
-            )
-          ],
-        ),
-      )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
